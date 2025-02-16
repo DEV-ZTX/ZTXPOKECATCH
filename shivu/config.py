7075fd3f72136a48c3a9d741d016e7d6a854505f
DEV-ZTX <sucidaldazai777@gmail.com>
2025-02-16 14:03:11 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -7,19 +7,19 @@ load_dotenv()
 class Config:
     LOGGER = True
 
-    OWNER_ID = int(os.getenv("OWNER_ID", "0"))  # Default to 0 if missing
+    OWNER_ID = int(os.getenv("OWNER_ID", ""))  # Default to 0 if missing
     SUDO_USERS = list(map(int, os.getenv("SUDO_USERS", "").split())) if os.getenv("SUDO_USERS") else []
     GROUP_ID = int(os.getenv("GROUP_ID", "-1002323856532"))
     TOKEN = os.getenv("TOKEN") or "YOUR_DEFAULT_TOKEN"
     MONGO_URL = os.getenv("MONGO_URL") or "YOUR_DEFAULT_MONGO_URL"
     PHOTO_URL = os.getenv("PHOTO_URL", "").split(",") if os.getenv("PHOTO_URL") else []
-    VIDEO_URL = os.getenv("VIDEO_URL", "https://example.com/specific_video.mp4")  # Add Video URL here
+    VIDEO_URL = os.getenv("VIDEO_URL", "")  # Add Video URL here
     SUPPORT_CHAT = os.getenv("SUPPORT_CHAT", "https://t.me/default_support")
     UPDATE_CHAT = os.getenv("UPDATE_CHAT", "https://t.me/default_updates")
-    BOT_USERNAME = os.getenv("BOT_USERNAME", "DefaultBot")
+    BOT_USERNAME = os.getenv("BOT_USERNAME", "PokemonGuessbot")
     CHARA_CHANNEL_ID = int(os.getenv("CHARA_CHANNEL_ID", "-1002323856532"))
     
-    API_ID = int(os.getenv("API_ID", "0"))
+    API_ID = int(os.getenv("API_ID", ""))
     API_HASH = os.getenv("API_HASH", "default_hash")
 
 class Production(Config):
