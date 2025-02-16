e42aa4873163ad28d3e4c987b187a5defdff90f4
DEV-ZTX <sucidaldazai777@gmail.com>
2025-02-16 12:45:03 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -1,24 +1,28 @@
-class Config(object):
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+class Config:
     LOGGER = True
 
-    # Get this value from my.telegram.org/apps
-    OWNER_ID = "7678359785"
-    sudo_users = ""
-    GROUP_ID = -1002323856532
-    TOKEN = "8111204267:AAE5iq9Z0gPE3g-n9fiCxWYmUwKklE9CPRE"
-    mongo_url = "mongodb+srv://vjimmy276:<GL83MueHjvCT32m3>@cluster0.pxamg.mongodb.net/?retryWrites=true&w=majority"
-    PHOTO_URL = ["https://graph.org/file/34ce254537a31ca2a788b-f8b59bff546af8dc58.jpg", "https://graph.org/file/0cd48cfd409203a00da74-2a919fe99dd1b238ec.jpg"]
-    SUPPORT_CHAT = "https://t.me/ZTX_HEADQUATERS"
-    UPDATE_CHAT = "https://t.me/TEAM_ZTX"
-    BOT_USERNAME = "PokemonGuessbot"
-    CHARA_CHANNEL_ID = "-1002323856532"
-    api_id = 28928028
+    OWNER_ID = os.getenv("OWNER_ID")
+    SUDO_USERS = os.getenv("SUDO_USERS", "").split()
+    GROUP_ID = int(os.getenv("GROUP_ID", "-1002323856532"))
+    TOKEN = os.getenv("TOKEN")
+    MONGO_URL = os.getenv("MONGO_URL")
+    PHOTO_URL = os.getenv("PHOTO_URL", "").split(",")
+    SUPPORT_CHAT = os.getenv("SUPPORT_CHAT")
+    UPDATE_CHAT = os.getenv("UPDATE_CHAT")
+    BOT_USERNAME = os.getenv("BOT_USERNAME")
+    CHARA_CHANNEL_ID = os.getenv("CHARA_CHANNEL_ID")
 
-    api_hash = "b097202e877124392f4851d215fa8f3a"
-    
+    API_ID = int(os.getenv("API_ID"))
+    API_HASH = os.getenv("API_HASH")
+
 class Production(Config):
     LOGGER = True
-
 
 class Development(Config):
     LOGGER = True
